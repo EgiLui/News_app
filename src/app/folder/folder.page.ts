b1c43d96dc7051a76f56859462035c42854e015b
Egidijus Luinevicius <egidijus.lu@protonmail.com>
2023-07-05 20:57:03 +0100

    News app

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NewsapiService } from '../services/newsapi.service';
+import { map } from 'rxjs/operators';
+import { ArticlesEntity } from '../interfaces/news-response';
 
 @Component({
   selector: 'app-folder',
@@ -8,10 +11,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  newsList: ArticlesEntity[] = [];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private newsApiService: NewsapiService
+  ) {
+    this.newsList = [];
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.getTopHeadLines();
+  }
+
+  getTopHeadLines() {
+    this.newsApiService
+      .getTopCountryHeadlines('us', this.folder)
+      .pipe(map((res) => res.articles))
+      .subscribe((news) => (this.newsList = news as ArticlesEntity[]));
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseURL: 'https://newsapi.org/v2/top-headlines',
+  apiKey: 'a46fde0a6e85455187e34c8cba9a61d8',
 };
 
 /*
